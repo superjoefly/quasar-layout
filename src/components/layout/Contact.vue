9378f2a5be2090812b08238ca481fad6f4c244b3
superjoefly <superjoefly@gmail.com>
2017-12-21 21:23:50 -0700

    initial commit

---
+++ src/components/layout/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="caption">Drawer</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+
+    }),
+    methods: {
+
+    },
+    computed: {
+
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/layout/Master.vue
@@ -0,0 +1,78 @@
+<template>
+  <!-- Configure "view" prop for QLayout -->
+  <q-layout ref="layout">
+    <q-toolbar slot="header">
+      <!-- opens drawer below -->
+      <button class="hide-on-drawer-visible" @click="$refs.layout.toggleLeft()">
+        <i>menu</i>
+      </button>
+
+      <q-toolbar-title>
+        Title
+      </q-toolbar-title>
+    </q-toolbar>
+
+    <!-- Navigation Tabs -->
+    <q-tabs slot="navigation">
+      <q-route-tab slot="title" icon="input" to="/home" replace label="Home" />
+      <q-route-tab slot="title" icon="view_quilt" to="/about" replace hide="icon" label="About" />
+      <q-route-tab slot="title" icon="view_day" to="/contact" replace hide="icon" label="Contact" />
+      <q-route-tab slot="title" icon="view_day" to="/more" replace label="More" />
+    </q-tabs>
+
+
+    <!-- Left Side Panel -->
+    <div slot="left">
+      <q-list no-border link inset-delimiter>
+        <q-list-header>Essential Links</q-list-header>
+        <q-item>
+          <q-item-side icon="school" />
+          <q-item-main label="Docs" sublabel="quasar-framework.org" />
+        </q-item>
+        <q-item>
+          <q-item-side icon="record_voice_over" />
+          <q-item-main label="Forum" sublabel="forum.quasar-framework.org" />
+        </q-item>
+        <q-item>
+          <q-item-side icon="chat" />
+          <q-item-main label="Gitter Channel" sublabel="Quasar Lobby" />
+        </q-item>
+        <q-item>
+          <q-item-side icon="rss feed" />
+          <q-item-main label="Twitter" sublabel="@quasarframework" />
+        </q-item>
+      </q-list>
+    </div>
+
+
+    <!-- Right Side Panel -->
+    <div slot="right">
+      <p>Right Panel</p>
+    </div>
+
+
+    <router-view />
+
+    <!-- Footer -->
+    <q-toolbar slot="footer">
+      <p>Footer</p>
+    </q-toolbar>
+
+  </q-layout>
+</template>
+
+<script>
+import { QLayout, QToolbar, QToolbarTitle, QTabs, QRouteTab, QList, QListHeader, QItem, QItemSide, QItemMain } from 'quasar'
+
+export default {
+  components: {
+    QLayout, QToolbar, QToolbarTitle, QTabs, QRouteTab, QList, QListHeader, QItem, QItemSide, QItemMain
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
